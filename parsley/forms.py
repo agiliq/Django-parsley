f336a7c5c31a540815e8b6b3150f784529cfa8b2
Shabda Raaj <shabda@agiliq.com>
2013-01-22 13:57:03 +0530

    Added tests to test that parsleyfy works as a class decorator.

--- parsley/forms.py
+++ parsley/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .decorators import parsleyfy
 
 class TextForm(forms.Form):
     name = forms.CharField(required=True)
+    university = forms.CharField(required=False)
+
+@parsleyfy
+class TextForm2(forms.Form):
+    name = forms.CharField(required=True)
+    university = forms.CharField(required=False)
--- parsley/tests.py
+++ parsley/tests.py
@@ -1,17 +1,26 @@
 from django.test import TestCase
 
-from .forms import TextForm
+from .forms import TextForm, TextForm2
 from .decorators import parsleyfy
 
 class CharFieldTest(TestCase):
     def test_basic(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that parsleyfy will add data-required for required fields,
+        but not for required=False fields for CharFields
         """
         form = TextForm()
         self.assertEqual(form.fields["name"].widget.attrs, {})
+        self.assertEqual(form.fields["university"].widget.attrs, {})
         ParsleyForm = parsleyfy(TextForm)
         form = ParsleyForm()
         self.assertEqual(form.fields["name"].widget.attrs, {"data-required": "true"})
+        self.assertEqual(form.fields["university"].widget.attrs, {})
+
+class CharFieldDecoratedTest(TestCase):
+    def test_decorated(self):
+        form = TextForm2()
+        self.assertEqual(form.fields["name"].widget.attrs, {"data-required": "true"})
+        self.assertEqual(form.fields["university"].widget.attrs, {})
 
 
