68b5e6fbae83210c90e3a571d54f1405839088fc
Javed Khan <javed@agiliq.com>
2013-08-16 15:52:39 +0530

    refactored settings

--- example/example/wsgi.py
+++ example/example/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings.local")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
