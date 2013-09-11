7a08bf0e7c4ef942328fe8906ef7c376d9c3ac31
Javed Khan <javed@agiliq.com>
2013-09-11 16:37:01 +0530

    better variable names

--- parsley/decorators.py
+++ parsley/decorators.py
@@ -7,29 +7,29 @@ def parsleyfy(klass):
 
     def new_init(self, *args, **kwargs):
         old_init(self, *args, **kwargs)
-        for key, val in self.fields.items():
-            if val.required:
-                val.widget.attrs.update({"data-required": "true"})
-            if isinstance(val, forms.URLField):
-                val.widget.attrs.update({"data-type": "url"})
-            if isinstance(val, forms.EmailField):
-                val.widget.attrs.update({"data-type": "email"})
-            if isinstance(val, forms.IntegerField):
-                val.widget.attrs.update({"data-type": "digits"})
-            if isinstance(val, forms.DecimalField):
-                val.widget.attrs.update({"data-type": "number"})
-            if isinstance(val, forms.FloatField):
-                val.widget.attrs.update({"data-type": "number"})
-            if isinstance(val, forms.RegexField):
-                val.widget.attrs.update({"data-regexp": val.regex.pattern})
-            if hasattr(val, "max_length") and val.max_length:
-                val.widget.attrs.update({"data-maxlength": val.max_length})
-            if hasattr(val, "min_length") and val.min_length:
-                val.widget.attrs.update({"data-minlength": val.min_length})
-            if hasattr(val, 'min_value') and val.min_value:
-                val.widget.attrs.update({'data-min': val.min_value})
-            if hasattr(val, 'max_value') and val.max_value:
-                val.widget.attrs.update({'data-max': val.max_value})
+        for _, field in self.fields.items():
+            if field.required:
+                field.widget.attrs.update({"data-required": "true"})
+            if isinstance(field, forms.URLField):
+                field.widget.attrs.update({"data-type": "url"})
+            if isinstance(field, forms.EmailField):
+                field.widget.attrs.update({"data-type": "email"})
+            if isinstance(field, forms.IntegerField):
+                field.widget.attrs.update({"data-type": "digits"})
+            if isinstance(field, forms.DecimalField):
+                field.widget.attrs.update({"data-type": "number"})
+            if isinstance(field, forms.FloatField):
+                field.widget.attrs.update({"data-type": "number"})
+            if isinstance(field, forms.RegexField):
+                field.widget.attrs.update({"data-regexp": field.regex.pattern})
+            if hasattr(field, "max_length") and field.max_length:
+                field.widget.attrs.update({"data-maxlength": field.max_length})
+            if hasattr(field, "min_length") and field.min_length:
+                field.widget.attrs.update({"data-minlength": field.min_length})
+            if hasattr(field, 'min_value') and field.min_value:
+                field.widget.attrs.update({'data-min': field.min_value})
+            if hasattr(field, 'max_value') and field.max_value:
+                field.widget.attrs.update({'data-max': field.max_value})
     klass.__init__ = new_init
 
     return klass
