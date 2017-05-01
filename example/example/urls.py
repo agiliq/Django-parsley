50ffb0cd692967707eb94bd8950b02d9995067c6
ramesh <hugarrames@gmail.com>
2017-05-02 00:31:37 +0530

    upgraded to django 1.10

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from django.conf.urls import patterns, url, include
+from django.conf.urls import include, url
 
 from core.views import HomeView
 
 admin.autodiscover()
 
 
-urlpatterns = patterns('core.views',
+urlpatterns =[
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomeView.as_view(), name='home'),
-)
+]
